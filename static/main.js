1938320c183e90b580b762e05f7f58f1a3afb9b7
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2020-02-22 16:07:55 +0530

    Mild refactoring, towards Nikola.

--- static/main.js
+++ static/main.js
@@ -1,70 +1,74 @@
-setInterval(updateTimes, 60000);
-updateTimes();
-document.body.addEventListener('click', onBodyClick);
+window.onload = main;
 
-if (document.cookie.match(/(^|;\s*)cookiesOk=1(;|$)/))
-	cookiesOkSave();  // Save again for it to expire in seven days from now.
-else
-	document.getElementById('cookiesOkBox').classList.remove('hide');
+function main() {
+	setInterval(updateTimes, 60000);
+	updateTimes();
+	document.body.addEventListener('click', onBodyClick);
 
-for (const el of document.querySelectorAll('sup.footnote-ref a'))
-	el.setAttribute('title',
-		document.getElementById(el.getAttribute('href').slice(1)).firstChild.textContent.trim());
+	if (document.cookie.match(/(^|;\s*)cookiesOk=1(;|$)/))
+		cookiesOkSave();  // Save again for it to expire in seven days from now.
+	else
+		document.getElementById('cookiesOkBox').classList.remove('hide');
 
-for (const el of document.querySelectorAll('div.hl')) {
-	const buttons = [];
-	if (el.dataset.lang == 'pycon')
-		buttons.push(`<button type=button class=console-toggle-btn
-			title="Hide output and '&gt;&gt;&gt;' prompt">&gt;&gt;&gt;</button>`);
-	if (el.dataset.lang == 'console')
-		buttons.push(`<button type=button class=console-toggle-btn
-			title="Hide output and prompt strings">$</button>`);
-	buttons.push('<button type=button class=copy-btn>Copy</button>');
-	el.insertAdjacentHTML('beforeEnd', '<div class=btns>' + buttons.join('\n') + '</div>');
-}
+	for (const el of document.querySelectorAll('sup.footnote-ref a'))
+		el.setAttribute('title',
+			document.getElementById(el.getAttribute('href').slice(1)).firstChild.textContent.trim());
 
-if (!localStorage.noga && !location.hostname.match(/localhost/)) {
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-	gtag('config', 'UA-105705354-1');
-	var s = document.createElement('script');
-	s.async = true;
-	s.src = '//www.googletagmanager.com/gtag/js?id=UA-105705354-1';
-	s.setAttribute('data-timestamp', +new Date());
-	document.body.appendChild(s);
-}
+	for (const el of document.querySelectorAll('div.hl')) {
+		const buttons = [];
+		if (el.dataset.lang == 'pycon')
+			buttons.push(`<button type=button class=console-toggle-btn
+				title="Hide output and '&gt;&gt;&gt;' prompt">&gt;&gt;&gt;</button>`);
+		if (el.dataset.lang == 'console')
+			buttons.push(`<button type=button class=console-toggle-btn
+				title="Hide output and prompt strings">$</button>`);
+		buttons.push('<button type=button class=copy-btn>Copy</button>');
+		el.insertAdjacentHTML('beforeEnd', '<div class=btns>' + buttons.join('\n') + '</div>');
+	}
 
-document.body.addEventListener('click', (event) => {
-	if (event.target.matches('a[href="#"]') || event.target.closest('a[href="#"]') !== null)
-		event.preventDefault();
-});
+	if (!localStorage.noga && !location.hostname.match(/localhost/)) {
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-105705354-1');
+		var s = document.createElement('script');
+		s.async = true;
+		s.src = '//www.googletagmanager.com/gtag/js?id=UA-105705354-1';
+		s.setAttribute('data-timestamp', +new Date());
+		document.body.appendChild(s);
+	}
 
-// Collapse large code blocks.
-for (const code of document.querySelectorAll('pre > code')) {
-	const lineCount = code.innerHTML.match(/\n/g).length;
-	if (lineCount < 25)
-		continue;
-	const previewPat = /^([^\n]*\n){20}/;
-	const match = code.innerHTML.match(previewPat);
-	if (!match)
-		continue;
-	code.dataset.fullMarkup = code.innerHTML;
-	code.innerHTML = code.innerHTML.substr(0, match[0].length);
-	code.insertAdjacentHTML('afterEnd',
-		'<button class=show-full-code-btn data-click=showFullCodeBlock>' +
-		`&darr; Show ${lineCount - 20} more lines</button>`);
+	document.body.addEventListener('click', (event) => {
+		if (event.target.matches('a[href="#"]') || event.target.closest('a[href="#"]') !== null)
+			event.preventDefault();
+	});
 
-	const table = code.closest('.hltable');
-	if (table) {
-		const lineNos = table.querySelector('.linenodiv pre');
-		lineNos.dataset.fullMarkup = lineNos.innerHTML;
-		lineNos.innerHTML = lineNos.innerHTML.substr(0, lineNos.innerHTML.match(previewPat)[0].length);
+	// Collapse large code blocks.
+	for (const code of document.querySelectorAll('pre > code')) {
+		const lineCount = code.innerHTML.match(/\n/g).length;
+		if (lineCount < 25)
+			continue;
+		const previewPat = /^([^\n]*\n){20}/;
+		const match = code.innerHTML.match(previewPat);
+		if (!match)
+			continue;
+		code.dataset.fullMarkup = code.innerHTML;
+		code.innerHTML = code.innerHTML.substr(0, match[0].length);
+		code.insertAdjacentHTML('afterEnd',
+			'<button class=show-full-code-btn data-click=showFullCodeBlock>' +
+			`&darr; Show ${lineCount - 20} more lines</button>`);
+
+		const table = code.closest('.hltable');
+		if (table) {
+			const lineNos = table.querySelector('.linenodiv pre');
+			lineNos.dataset.fullMarkup = lineNos.innerHTML;
+			lineNos.innerHTML = lineNos.innerHTML.substr(0, lineNos.innerHTML.match(previewPat)[0].length);
+		}
 	}
 }
 
 function updateTimes() {
-	for (const el of document.getElementsByTagName('time')) {
+	for (const el of document.querySelectorAll('time[data-show-relative]')) {
 		el.title = el.title || el.textContent;
 		el.textContent = timeSince(Date.parse(el.getAttribute('datetime')));
 	}
@@ -134,4 +138,14 @@ function showFullCodeBlock(event) {
 		const lineNos = table.querySelector('.linenodiv pre');
 		lineNos.innerHTML = lineNos.dataset.fullMarkup;
 	}
+}
+
+function loadComments(event) {
+	event.target.remove();
+	document.getElementById('disqus_thread').innerHTML = 'Loading comments&hellip;';
+	const s = document.createElement('script');
+	s.async = true;
+	s.src = '//sharats-me.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	document.body.appendChild(s);
 }
